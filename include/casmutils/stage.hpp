e47e53f62355eb00b3874556e6911df005b37c5f
Jonas Kaufman <Jonas@ResNet-44-225.resnet.ucsb.edu>
2019-05-15 11:27:48 -0700

    layerskewer impl continues

--- include/casmutils/stage.hpp
+++ include/casmutils/stage.hpp
@@ -18,11 +18,14 @@ public:
     LayerSkewer(Rewrap::Structure prim_struc, Rewrap::Structure super_struc, const int prim_stacking_vec_index=2, const int super_stacking_vec_index=2);
 
     /// Skew the superstructure by multiples of the in-layer lattice vectors
-    void skew_super_struc(const int a_multiple, const int b_multiple);
+    Rewrap::Structure skew_super_struc(const int a_multiple, const int b_multiple);
 
     /// Generate all skewed versions of superstructure
-    /// Only distinct one by default
+    /// Only distinct ones by default
     void generate_skewed_strucs(const bool distinct_only=true);
+
+    /// Return a vector of your skewed structures
+    std::vector<Rewrap::Structure> get_skewed_strucs();
 
 private:
     Rewrap::Structure prim_struc;
@@ -30,8 +33,9 @@ private:
     const int prim_stacking_vec_index;
     const int super_stacking_vec_index;
     std::vector<Rewrap::Structure> skewed_strucs;
-    
-    /// skew_lattice();
+
+    /// Add a new struc to skewed_strucs only if it is distinct
+    void add_if_distinct(Rewrap::Structure candidate_struc);
 
 };
 
--- src/casmutils/stage.cxx
+++ src/casmutils/stage.cxx
@@ -1,11 +1,11 @@
 #include "casmutils/stage.hpp"
 #include <casm/crystallography/Structure.hh>
-#include <casm/clex/ScelEnum.hh>
-#include <casm/crystallography/SupercellEnumerator.hh>
+//#include <casm/clex/ScelEnum.hh>
+//#include <casm/crystallography/SupercellEnumerator.hh>
 #include "casmutils/exceptions.hpp"
 #include "casmutils/structure.hpp"
 #include "casmutils/lattice.hpp"
-#include <casm/crystallography/Niggli.hh>
+//#include <casm/crystallography/Niggli.hh>
 
 namespace Simplicity
 {
@@ -15,14 +15,44 @@ LayerSkewer::LayerSkewer(Rewrap::Structure prim_struc, Rewrap::Structure super_s
         skewed_strucs.push_back(super_struc);        
     }
 
-void LayerSkewer::skew_super_struc(const int a_multiple, const int b_multiple)
+Rewrap::Structure LayerSkewer::skew_super_struc(const int a_multiple, const int b_multiple)
     {
-        return;
+        return super_struc;
     }
 
 void LayerSkewer::generate_skewed_strucs(const bool distinct_only)
     {
+        // loop over prim grid and generate new structures using skew_super_struc
+        
+        Rewrap::Structure new_struc = skew_super_struc(0,0);
+
+        if (distinct_only) {
+            add_if_distinct(new_struc);
+        } else {
+            skewed_strucs.push_back(new_struc);
+        }
+ 
         return;
     }
-    
+
+std::vector<Rewrap::Structure> LayerSkewer::get_skewed_strucs()
+    {
+        return skewed_strucs;
+    }
+
+void LayerSkewer::add_if_distinct(Rewrap::Structure candidate_struc)
+    {
+        // get vector of scores from struc_score using candidate_struc as prim
+
+        // check their weights
+        
+        // add if below threshold
+        if (false) {
+            skewed_strucs.push_back(candidate_struc);
+        }
+
+        return;
+    }
+
+
 }
