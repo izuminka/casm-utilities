7b2d040a424c96b6f7deeb789fab78113c6b4a8c
Jonas Kaufman <Jonas@169-231-125-37.wireless.ucsb.edu>
2019-05-15 15:14:01 -0700

    impl skew_super_struc, not tested

--- include/casmutils/stage.hpp
+++ include/casmutils/stage.hpp
@@ -14,10 +14,11 @@ public:
     LayerSkewer() = delete;
 
     /// Construct by providing primitive structure and superstructure (assumed to be layered)
-    /// Optionally can specify the index of the out-of-layer lattice vector (default is third)
-    LayerSkewer(Rewrap::Structure prim_struc, Rewrap::Structure super_struc, const int prim_stacking_vec_index=2, const int super_stacking_vec_index=2);
+    /// Optionally can specify the index of the out-of-layer lattice vector for each structure, called c (default is third)
+    LayerSkewer(Rewrap::Structure prim_struc, Rewrap::Structure super_struc, const int prim_c_vec_index=2, const int super_c_vec_index=2);
 
-    /// Skew the superstructure by multiples of the in-layer lattice vectors
+    /// Skew the superstructure by multiples of the in-layer lattice vectors of the prim, called a and b
+    /// a and b are defined in a cyclic manner based on the index of c (i.e. 0->a,1->b,2->c or 0->c,1->a,2->b or 0->b,1->c,2->a)
     Rewrap::Structure skew_super_struc(const int a_multiple, const int b_multiple);
 
     /// Generate all skewed versions of superstructure
@@ -30,8 +31,8 @@ public:
 private:
     Rewrap::Structure prim_struc;
     Rewrap::Structure super_struc;
-    const int prim_stacking_vec_index;
-    const int super_stacking_vec_index;
+    const int prim_c_vec_index;
+    const int super_c_vec_index;
     std::vector<Rewrap::Structure> skewed_strucs;
 
     /// Add a new struc to skewed_strucs only if it is distinct
--- src/casmutils/stage.cxx
+++ src/casmutils/stage.cxx
@@ -10,18 +10,33 @@
 namespace Simplicity
 {
     
-LayerSkewer::LayerSkewer(Rewrap::Structure prim_struc, Rewrap::Structure super_struc, const int prim_stacking_vec_index, const int super_stacking_vec_index): prim_struc(prim_struc), super_struc(super_struc), prim_stacking_vec_index(prim_stacking_vec_index), super_stacking_vec_index(super_stacking_vec_index)
+LayerSkewer::LayerSkewer(Rewrap::Structure prim_struc, Rewrap::Structure super_struc, const int prim_c_vec_index, const int super_c_vec_index): prim_struc(prim_struc), super_struc(super_struc), prim_c_vec_index(prim_c_vec_index), super_c_vec_index(super_c_vec_index)
     {
-        skewed_strucs.push_back(super_struc);        
+        skewed_strucs.push_back(super_struc); // not sure the superstructure should be added initially        
     }
 
 Rewrap::Structure LayerSkewer::skew_super_struc(const int a_multiple, const int b_multiple)
     {
-        return super_struc;
+        // Get lattices
+        Eigen::Matrix3d prim_lat_mat = prim_struc.lattice().lat_column_mat();
+        Eigen::Matrix3d super_lat_mat = super_struc.lattice().lat_column_mat(); 
+
+        // Update c lattice parameter
+        int prim_a_vec_index = (prim_c_vec_index - 2) % 3;
+        int prim_b_vec_index = (prim_c_vec_index - 1) % 3;
+        super_lat_mat.col(super_c_vec_index) = super_lat_mat.col(super_c_vec_index) + a_multiple*prim_lat_mat.col(prim_a_vec_index) + b_multiple*prim_lat_mat.col(prim_b_vec_index);
+
+        // Create new structure with the skewed lattice 
+        Rewrap::Structure new_struc = super_struc;
+        CASM::Lattice new_lat(super_lat_mat);
+        new_struc.set_lattice(new_lat, CASM::CART);
+
+        return new_struc;
     }
 
 void LayerSkewer::generate_skewed_strucs(const bool distinct_only)
     {
+        // TODO
         // loop over prim grid and generate new structures using skew_super_struc
         
         Rewrap::Structure new_struc = skew_super_struc(0,0);
@@ -42,6 +57,7 @@ std::vector<Rewrap::Structure> LayerSkewer::get_skewed_strucs()
 
 void LayerSkewer::add_if_distinct(Rewrap::Structure candidate_struc)
     {
+        // TODO
         // get vector of scores from struc_score using candidate_struc as prim
 
         // check their weights
