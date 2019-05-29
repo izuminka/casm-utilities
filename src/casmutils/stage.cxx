c9e0fd34676f3d5517f0d609b3bb9cf82bcb2c92
John Goiri <jg.goiri@gmail.com>
2019-05-29 16:39:49 -0700

    Meeting changes

--- src/casmutils/stage.cxx
+++ src/casmutils/stage.cxx
@@ -1,43 +1,46 @@
-#include "include/casmutils/structure.hpp"
 #include "include/casmutils/stage.hpp"
 #include "casm/crystallography/Lattice.hh"
 #include "casm/crystallography/Structure.hh"
+#include "include/casmutils/structure.hpp"
 
+void symmetrize(const Rewrap::Structure* struc_ptr, double tol)
+{
+    auto& sym_struc=*struc_ptr;
+    // Use this to make sure you're getting a group size that makes sense later
+    std::set<int> point_group_sizes{1, 2, 3, 4, 6, 8, 12, 16, 24, 48};
 
+    // a) symmetrize the lattice vectors
+    CASM::Lattice lat = sym_struc.lattice();
+    lat.symmetrize(tol);
+    // lat.set_tol(tol);
+    CASM::SymGroup pg;
+    // TODO: change interface within Rewrap for this call
+    lat.generate_point_group(pg);
+
+    // Your group size needs to be one of the values in the set at the top
+    assert(point_group_sizes.find(pg.size()) != point_group_sizes.end());
+
+    // Now the structure will have the symmetrized lattice
+    sym_struc.set_lattice(lat, CASM::FRAC);
+
+    sym_struc.generate_factor_group(tol);
+    // b) find factor group with same tolerance
+    sym_struc.fg_converge(tol); // TODO:  Is this for real necessary? Doesn't generate_factor_group do this?
+
+    // Your group size needs to be one of the values in the set at the top
+    assert(point_group_sizes.find(sym_struc.point_group().size()) != point_group_sizes.end());
+
+    // c) symmetrize the basis sites
+    sym_struc.symmetrize(sym_struc.factor_group());
+    int biggest = struc.factor_group().size();
+    assert(sym_struc.factor_group().is_group(tol) && (sym_struc.factor_group().size() >= biggest));
+
+    return;
+}
 
 Rewrap::Structure symmetrize(const Rewrap::Structure& struc, double tol)
 {
-    std::set<int> point_group_sizes {1,2,3,4,6,8,12,16,24,48};
-    Rewrap::Structure sym_struc=struc;
-    int biggest = struc.factor_group().size();
-    CASM::Structure tmp = struc;
-    // a) symmetrize the lattice vectors
-    CASM::Lattice lat = tmp.lattice();
-    lat.symmetrize(tol);
-    //lat.set_tol(tol);
-    CASM::SymGroup pg;
-    lat.generate_point_group(pg);
-    if (point_group_sizes.find(pg.size()) == point_group_sizes.end())
-    {
-        return struc;
-    }
-    tmp.set_lattice(lat, CASM::FRAC);
-
-    tmp.factor_group();
-    // b) find factor group with same tolerance
-    tmp.fg_converge(tol);
-    if (point_group_sizes.find(tmp.point_group().size()) == point_group_sizes.end())
-    {
-        return struc;
-    }
-
-    // c) symmetrize the basis sites
-    tmp.symmetrize(tmp.factor_group());
-    if (tmp.factor_group().is_group(tol) && (tmp.factor_group().size() > biggest))
-    {
-        sym_struc=tmp;
-        biggest = tmp.factor_group().size();
-    }
-    return sym_struc;
+    auto mutable_struc=struc;
+    symmetrize(&mutable_struc, tol);
+    return mutable_struc;
 }
-
